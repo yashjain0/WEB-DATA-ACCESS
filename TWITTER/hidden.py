eea58b422081d0fbcd3bac8ada80d48de9294c87
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-05-05 18:45:38 +0530

    authentication keys to find

--- TWITTER/hidden.py
+++ TWITTER/hidden.py
@@ -5,6 +5,7 @@
 
 def oauth():
     return {"consumer_key": "SABsCPosQmw2IDYSFXvAaExyV",
-            "consumer_secret": "2L7yQtq0v1X9IN40Cj191kzpwOZGQOqLCZSrDhL1yflXcDzQ14",
-            "token_key": "817079919029211136-TBvJobWQ3U230hoZgGKJcA0KkXQTPtH",
-            "token_secret": "WAvwV68rG1S0oakeUX87EAEJJKLCydyEGGPduDg8tEWQD"}
+            "consumer_secret": "2L7yQtq0v1X9IN40Cj191kzpwOZGQOqLCZ***hL1yflXcDzQ14",
+            "token_key": "817079919029211136-TBvJobWQ3U230hoZgGKJcA0***QTPtH",
+            "token_secret": "WAvwV68rG1S0oakeUX87EAEJJKLCydyEGGP***8tEWQD"}
+#not have complete keys have to take from hidden.py from hard disk.
