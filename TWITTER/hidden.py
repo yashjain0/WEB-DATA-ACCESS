aab748a0442428c85b4f9480759c1ac0e5a98f44
Yash Jain <62240937+yashjain0@users.noreply.github.com>
2021-05-05 18:41:44 +0530

    Contains authentiction strings

---
+++ TWITTER/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "SABsCPosQmw2IDYSFXvAaExyV",
+            "consumer_secret": "2L7yQtq0v1X9IN40Cj191kzpwOZGQOqLCZSrDhL1yflXcDzQ14",
+            "token_key": "817079919029211136-TBvJobWQ3U230hoZgGKJcA0KkXQTPtH",
+            "token_secret": "WAvwV68rG1S0oakeUX87EAEJJKLCydyEGGPduDg8tEWQD"}
